a406992e6830563675b3d93d67f16d7756edb948
Danylo Nechai <danylo.nechai@heureka.cz>
2021-10-27 13:53:04 +0200

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='aio-py-rq',
-    version='2.0.0',
+    version='2.0.1',
     packages=['aiopyrq'],
     url='https://github.com/heureka/aio-py-rq',
     license='Apache 2.0',
