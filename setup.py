654b2b76ff84f88b1a2d429ef5e98bc9dbfc5d50
vladimir.kaspar <vladimir.kaspar@heureka.cz>
2024-02-16 14:55:26 +0100

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='aio-py-rq',
-    version='3.1.0',
+    version='4.0.0',
     packages=['aiopyrq'],
     url='https://github.com/heureka/aio-py-rq',
     license='Apache 2.0',
